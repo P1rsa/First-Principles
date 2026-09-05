bd45b1a0cd3c5acda81c5fc8218a144b2080c10e
P1rsa <157224677+P1rsa@users.noreply.github.com>
2026-09-05 13:33:40 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 (function () {
   'use strict';
 
-  */
+  
   const BOOKING_ENDPOINT = 'https://script.google.com/macros/s/AKfycbxWj4-3U4RxOR2KXvSzNhxylnRAm0UmplhyyA08WnM4v5ES_-O7BX9abn0ZOSZDL-Z5/exec';
 
   /* ============================================
